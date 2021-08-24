bf7ae8d8f163b88174a90795e8629aa23d270b33
kvt_512 <vishnu.koyya@glarimy.com>
2021-08-24 13:57:14 -0700

    2nd development day
    1. Added 2 new scenes
    2. Programmed the loading scene
    3. Imported Sounds, Images and TextMeshPro

---
+++ Glitch Garden/Assets/Script/SceneLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    int currentScene;
+    float timeToWait = 4F;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentScene == 0) {
+            StartCoroutine(WaitAndLoadNextScene());
+        }   
+    }
+
+    IEnumerator WaitAndLoadNextScene() {
+        yield return new WaitForSeconds(timeToWait);
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
