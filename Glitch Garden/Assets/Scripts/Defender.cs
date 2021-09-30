6f89850ca7c4a7a08e64b1e2c140741f76724eb4
kvt_512 <vishnu.koyya@glarimy.com>
2021-09-29 18:14:03 -0700

    Added VFX and SFX for projectile and Lizard

--- Glitch Garden/Assets/Scripts/Defender.cs
+++ Glitch Garden/Assets/Scripts/Defender.cs
@@ -6,7 +6,14 @@ public class Defender : MonoBehaviour
 {
     [SerializeField] GameObject zucchini;
     [SerializeField] Transform firePoint;
+    [SerializeField] AudioClip projectileReleaseSFX;
+    float projectileReleaseSFXVol = 10F;
     private void Shoot() {
         GameObject zucchiniGameObject = Instantiate(zucchini, firePoint.position, Quaternion.identity);
+        TriggerProjectileReleaseSFX();
+    }
+
+    private void TriggerProjectileReleaseSFX() {
+        AudioSource.PlayClipAtPoint(projectileReleaseSFX, Camera.main.transform.position, projectileReleaseSFXVol);
     }
 }
--- Glitch Garden/Assets/Scripts/Health.cs
+++ Glitch Garden/Assets/Scripts/Health.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] float startingHealth = 100F;
+    [SerializeField] GameObject explosionVFX;
+    [SerializeField] AudioClip explosionSFX;
+    float explosionSFXVol = 1F;
+    float destoryExplosionVFXDelay = 1F;
     float noHealth = 0;
     // Start is called before the first frame update
     void Start()
@@ -21,7 +26,18 @@ public class Health : MonoBehaviour
     public void DealDamage(float damage) {
         startingHealth = startingHealth - damage;
         if (startingHealth <= noHealth) {
+            TriggerExplosionVFX();
             Destroy(this.gameObject);
+            TriggerExplosionSFX();
         } 
+    }
+
+    private void TriggerExplosionVFX() {
+        GameObject explosionVFXGameObject =  Instantiate(explosionVFX, this.transform.position, Quaternion.identity) as GameObject;
+        Destroy(explosionVFXGameObject, destoryExplosionVFXDelay);
+    }
+
+    private void TriggerExplosionSFX() {
+        AudioSource.PlayClipAtPoint(explosionSFX, Camera.main.transform.position, explosionSFXVol);
     }
 }
